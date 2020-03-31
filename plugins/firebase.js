f3d9cda674d198e8d3b22a09b1b995a9c5790b23
Dhea Aria Rachman <dheariarachman@gmail.com>
2020-03-31 21:35:32 +0700

    update initialized

--- plugins/firebase.js
+++
@@ -1,8 +0,0 @@
-import firebase from 'firebase'
-
-let app = null
-
-// Initialize firebase config
-app = firebase.apps.length ? firebase.app() : firebase.initializeApp(firebaseConfig);
-
-export const fireDb = app.firestore();
